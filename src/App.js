9ffa60a7db8be05929cd6419af31519ec31beaad
Matt Andrews <matt@carbonbasedcreative.com>
2020-01-10 06:21:34 -0700

    Added data source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { BREWERIESDB } from './shared/breweriesdb';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import Footer from './components/footer/footer.component';
@@ -10,11 +11,12 @@ class App extends Component {
     super(props);
 
     this.state = {
-
+      breweries: BREWERIESDB
     };
 
   }
   render() {
+    console.log(BREWERIESDB);
     return (
       <div className='container-fluid'>
           <Header />
---
+++ src/shared/breweriesdb.js
@@ -0,0 +1,12 @@
+export const BREWERIESDB = [
+    {
+        id: 0,
+        name: "Brewery Name",
+        image: "",
+        lon: "",
+        lat: "",
+        elevation: 1233,
+        featured: false,
+        description: "Text Description"
+    }
+];
