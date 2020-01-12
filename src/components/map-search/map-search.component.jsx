13a6b84b811933fbb84f452a167e2c58a578da49
Matt Andrews <matt@carbonbasedcreative.com>
2020-01-12 13:44:21 -0700

    Added rudimentary mapping functionality

---
+++ src/components/map-search/map-search.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './map-search.styles.scss';
+
+const MapSearch = () => {
+    return(
+        <footer>Map Search</footer>
+    );
+}
+
+export default MapSearch;
--- src/pages/map/map.component.jsx
+++ src/pages/map/map.component.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapSearch from '../../components/map-search/map-search.component';
 import './map.styles.scss';
 
-const Map = () => {
-    return(
-        <section className='map'>
-            <h1>Map</h1>
-        </section>
-    );
+class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: 5,
+            lat: 34,
+            zoom: 2
+        };
+    }
+
+    componentDidMount() {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic2VsY2VldXMiLCJhIjoiY2s0Mzh1NjFvMDJvcDNlbmFkejMyMHdjNyJ9.t-rxQRKpzYDBstSg-_QSUQ';
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom
+        });
+
+        map.on('move', () => {
+            this.setState({
+                lng: map.getCenter().lng.toFixed(4),
+                lat: map.getCenter().lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            });
+        });
+    }
+
+    render() {
+        return(
+            <section className='map'>
+                <div ref={el => this.mapContainer = el} className="mapContainer" />
+                <aside className='sidebarStyle'>
+                    <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+                </aside>
+                <MapSearch />
+            </section>
+        );
+    }
 }
 
-export default Homepage;
+export default Map;
