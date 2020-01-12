306a2275ec9580660302972c88ce759a8e5b8e9b
Matt Andrews <matt@carbonbasedcreative.com>
2020-01-12 16:19:17 -0700

    Implementing user geolocation

---
+++ src/components/utilities/spinner.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Spinner } from 'reactstrap';
+
+const Spinner = props => {
+    return (
+        <div>
+           <Spinner style={{ width: '3rem', height: '3rem' }} />{' '} 
+        </div>
+    );
+};
+
+export default Spinner;
--- src/pages/map/map.component.jsx
+++ src/pages/map/map.component.jsx
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
 import MapSearch from '../../components/map-search/map-search.component';
+import Tooltip from '../../components/map-tooltip/map-tooltip.component';
 import './map.styles.scss';
 
 class Map extends Component {
+
+    tooltipContainer;
+
     constructor(props) {
         super(props);
         this.state = {
-            lng: 5,
-            lat: 34,
-            zoom: 2
+            lng: -105.358887,
+            lat: 39.113014,
+            zoom: 8
         };
     }
 
+    setTooltip(features) {
+        if (features.length) {
+            ReactDOM.render(
+                React.createElement(
+                    Tooltip, {
+                    features
+                    }
+                ),
+                this.tooltipContainer
+            );
+        } else {
+            this.tooltipContainer.innerHTML = '';
+        }
+    }
+
     componentDidMount() {
+
+        this.tooltipContainer = document.createElement('div');
+
         mapboxgl.accessToken = 'pk.eyJ1Ijoic2VsY2VldXMiLCJhIjoiY2s0Mzh1NjFvMDJvcDNlbmFkejMyMHdjNyJ9.t-rxQRKpzYDBstSg-_QSUQ';
         const map = new mapboxgl.Map({
             container: this.mapContainer,
@@ -22,13 +45,21 @@ class Map extends Component {
             zoom: this.state.zoom
         });
 
-        map.on('move', () => {
+        const tooltip = new mapboxgl.Marker(this.tooltipContainer, { offset: [-120, 0] }).setLngLat([0,0]).addTo(map);
+
+        map.on('move', (e) => {
             this.setState({
                 lng: map.getCenter().lng.toFixed(4),
                 lat: map.getCenter().lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2)
+                zoom: map.getZoom().toFixed(2),
             });
         });
+
+        const features = map.queryRenderedFeatures(point);
+            
+        tooltip.setLngLat([-104.819800,38.835220]);
+        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+        this.setTooltip(features);
     }
 
     render() {
