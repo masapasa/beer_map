7bd1446c530dbf4ecd77b34a404571247d87789a
Matt Andrews <matt@carbonbasedcreative.com>
2020-01-12 19:45:16 -0700

    Added react-map-gl package - set up map with geolocation

--- src/pages/map/map.component.jsx
+++ src/pages/map/map.component.jsx
@@ -1,74 +1,66 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl';
+import MapGL, { GeolocateControl, NavigationControl, Marker } from 'react-map-gl';
 import MapSearch from '../../components/map-search/map-search.component';
-import Tooltip from '../../components/map-tooltip/map-tooltip.component';
+import Pin from '../../components/map-utilities/pin.component';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './map.styles.scss';
 
-class Map extends Component {
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoic2VsY2VldXMiLCJhIjoiY2s0Mzh1NjFvMDJvcDNlbmFkejMyMHdjNyJ9.t-rxQRKpzYDBstSg-_QSUQ';
 
-    tooltipContainer;
+const geolocateStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    margin: 10,
+    overflow: 'hidden'
+  };
+
+class Map extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            lng: -105.358887,
-            lat: 39.113014,
-            zoom: 8
+            viewport: {
+              latitude: 39.113014,
+              longitude: -105.358887,
+              zoom: 7,
+              bearing: 0,
+              pitch: 0
+            }
         };
     }
 
-    setTooltip(features) {
-        if (features.length) {
-            ReactDOM.render(
-                React.createElement(
-                    Tooltip, {
-                    features
-                    }
-                ),
-                this.tooltipContainer
-            );
-        } else {
-            this.tooltipContainer.innerHTML = '';
-        }
-    }
-
-    componentDidMount() {
-
-        this.tooltipContainer = document.createElement('div');
-
-        mapboxgl.accessToken = 'pk.eyJ1Ijoic2VsY2VldXMiLCJhIjoiY2s0Mzh1NjFvMDJvcDNlbmFkejMyMHdjNyJ9.t-rxQRKpzYDBstSg-_QSUQ';
-        const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
-        });
-
-        const tooltip = new mapboxgl.Marker(this.tooltipContainer, { offset: [-120, 0] }).setLngLat([0,0]).addTo(map);
-
-        map.on('move', (e) => {
-            this.setState({
-                lng: map.getCenter().lng.toFixed(4),
-                lat: map.getCenter().lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2),
-            });
-        });
-
-        const features = map.queryRenderedFeatures(point);
-            
-        tooltip.setLngLat([-104.819800,38.835220]);
-        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
-        this.setTooltip(features);
-    }
+    _onViewportChange = viewport => this.setState({viewport});
 
     render() {
+
+        const {viewport} = this.state;
+
         return(
             <section className='map'>
-                <div ref={el => this.mapContainer = el} className="mapContainer" />
-                <aside className='sidebarStyle'>
-                    <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
-                </aside>
+                <MapGL
+                    {...viewport}
+                    width="95vw"
+                    height="100vh"
+                    mapStyle="mapbox://styles/mapbox/light-v10"
+                    onViewportChange={this._onViewportChange}
+                    mapboxApiAccessToken={MAPBOX_TOKEN}
+                >
+                    <Marker latitude={39.113014} longitude={-105.358887} offsetLeft={-20} offsetTop={-10}>
+                    <h2><Pin />Hello</h2>
+                    </Marker>
+                    <div style={{position: 'absolute', top: 10, right: 10}}>
+                    <NavigationControl />
+                    </div>
+                    <GeolocateControl
+                        style={geolocateStyle}
+                        positionOptions={{enableHighAccuracy: true}}
+                        fitBoundsOptions={{maxZoom: 8}}
+                        label={"You are Here"}
+                        trackUserLocation={true}
+                    />
+
+                </MapGL>
                 <MapSearch />
             </section>
         );
