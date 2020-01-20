3f169fa754a2a91b225228ca7652df3290e2c4ac
Matt Andrews <matt@carbonbasedcreative.com>
2020-01-19 19:07:21 -0700

    Added additional page components

---
+++ src/pages/city-listing/city-listing.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './city-listing.styles.scss';
+
+const CityListing = () => {
+    return(
+        <h1>City Listing</h1>
+    );
+}
+
+export default CityListing;
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './contact.styles.scss';
+
+const Contact = () => {
+    return(
+        <h1>Contact</h1>
+    );
+}
+
+export default Contact;
